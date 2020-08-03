9697a53222624f2f9ad1c7951f175980b4e200a7
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-08-04 01:42:06 +0300

    alpha 0.2

--- main.py
+++ main.py
@@ -30,26 +30,55 @@ async def start(message : types.Message):
 
     button_search = KeyboardButton('Начать поиск🔍')
 
+    button_info_project = KeyboardButton('О проекте🧑‍💻')
+
+    rules = KeyboardButton('Правила📖')
+
     mark_menu = ReplyKeyboardMarkup()
 
-    mark_menu.add(button_search)
+    mark_menu.add(button_search,button_info_project,rules)
 
     await bot.send_message(message.chat.id,'Привет!\n\nЭто Гомельский Анонимный чат для пожилых кролов....\nкхм шучу\n\n',reply_markup=mark_menu)
 
+@dp.message_handler(lambda message : message.text == 'О проекте🧑‍💻' or message.text == 'Все ссылки на нас' or message.text == '[ Для разработчиков ]',state='*')
+async def about_project(message : types.Message):
+    if message.text == 'О проекте🧑‍💻':
+        links = KeyboardButton('Все ссылки на нас')
 
+        for_developers = KeyboardButton('[ Для разработчиков ]')
+
+        back = KeyboardButton('Назад')
+
+        mark_menu = ReplyKeyboardMarkup()
+
+        mark_menu.add(links,for_developers,back)
+
+        await bot.send_message(message.chat.id,'Вся информация тут👇',reply_markup=mark_menu)
+    elif message.text == 'Все ссылки на нас':
+        await message.answer('Главный разработчик - Якублевич Ренат\nСотрудничество - merlinincorp@gmail.com\n\nGithub - https://github.com/RenatYakublevich/AnonymChat')
+
+    elif message.text == '[ Для разработчиков ]':
+        await message.answer('Если вы разработчик и хотите поучаствовать в разработке проекта то смело контрибутье на гите или пишите на почту - merlinincorp@gmail.com')
+
+@dp.message_handler(commands=['rules'],state='*')
+@dp.message_handler(lambda message : message.text == 'Правила📖')
+async def rules(message : types.Message):
+    await message.answer('''📌Правила общения в @GomelAnonymChatBot\n1. Любые упоминания психоактивных веществ. (наркотиков)\n2. Детская порнография. ("ЦП")\n3. Мошенничество. (Scam)\n4. Любая реклама, спам.\n5. Продажи чего либо. (например - продажа интимных фотографий, видео)\n6. Любые действия, нарушающие правила Telegram.\n7. Оскорбительное поведение.\n8. Обмен, распространение любых 18+ материалов\n\n❌- За нарушение правил - блокировка аккаунта.''')
 
 @dp.message_handler(lambda message: message.text == 'Начать поиск🔍',state='*')
 async def search(message : types.Message):
-    if(not db.user_exists(message.from_user.id)):
-        db.add_user(message.from_user.username,message.from_user.id)
+    if(not db.user_exists(message.from_user.id)): #если пользователя с таким telegram id не найдено
+        db.add_user(message.from_user.username,message.from_user.id) #добавляем юзера в табличку дб
 
     male = KeyboardButton('Парня')
 
     wooman = KeyboardButton('Девушку')
 
+    back = KeyboardButton('Назад')
+
     sex_menu = ReplyKeyboardMarkup()
 
-    sex_menu.add(male,wooman)
+    sex_menu.add(male,wooman,back)
 
     await message.answer('Выбери пол собеседника!\nКого вы ищите?)',reply_markup=sex_menu)
 
@@ -80,9 +109,9 @@ async def chating(message : types.Message, state: FSMContext):
 
 
 
-
-
-
-
+#хендлер для команды назад
+@dp.message_handler(lambda message : message.text == 'Назад')
+async def back(message : types.Message):
+    await start(message)
 
 executor.start_polling(dp, skip_updates=True)
